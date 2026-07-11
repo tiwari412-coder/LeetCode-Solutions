6259975c63be07169b8a904307b37f7558200f1c
tiwari412-coder <bcstiwari412@gmail.com>
2026-07-12 00:41:08 +0530

    Time: 3 ms (32.4%), Space: 11.5 MB (17.41%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        result.push_back({});
+
+        for(int i=0;i<nums.size();i++){
+            int n = result.size();
+
+            for(int j=0;j<n;j++){
+                vector<int>temp = result[j];
+                temp.push_back(nums[i]);
+                result.push_back(temp);
+            }
+        }
+
+        set<vector<int>> st(result.begin(),result.end());
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+    }
+
+};
