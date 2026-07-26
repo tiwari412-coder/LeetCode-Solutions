97e5a6ce9d6103c3434165f1f7580d37b119375f
tiwari412-coder <bcstiwari412@gmail.com>
2026-07-26 23:59:51 +0530

    Time: 24 ms (33.74%), Space: 17.3 MB (84.91%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
+
+        for(int i=0;i<n-3; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                 continue;
+            }
+
+            for(int j=i+1; j<n-2; j++){
+                if(j > i+1 && nums[j] == nums[j-1]){
+                     continue;
+                }
+
+                int k =  j+1;
+                int m = nums.size()-1;
+
+                while(k<m){
+                    long long sum = 1LL*nums[i] + nums[j] + nums[k] + nums[m];
+
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[m]});
+                        k++;
+                        m--;
+
+                        while(k<m && nums[k] == nums[k-1]){
+                            k++;
+                        } 
+                        while(k<m && nums[m] == nums[m+1]){
+                            m--;
+                        }
+                    }
+                    else if(sum<target) k++;
+                    else m--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
