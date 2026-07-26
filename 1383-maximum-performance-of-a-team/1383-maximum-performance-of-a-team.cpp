0c7e7f8989063ee03d4f05651b1453fcb8f1d2d5
tiwari412-coder <bcstiwari412@gmail.com>
2026-07-27 00:51:33 +0530

    Time: 21 ms (61.61%), Space: 44.2 MB (22.7%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        
+
         vector<pair<int, int>> engineers;
-        
+
+        // Store (efficiency, speed)
         for (int i = 0; i < n; i++) {
             engineers.push_back({efficiency[i], speed[i]});
         }
-        
-        sort(engineers.rbegin(), engineers.rend()); // sort by efficiency descending
-        
+
+        // Sort in descending order of efficiency
+        sort(engineers.rbegin(), engineers.rend());
+
+        // Min Heap to store speeds
         priority_queue<int, vector<int>, greater<int>> minHeap;
-        
+
         long long speedSum = 0;
         long long ans = 0;
-        const int MOD = 1e9 + 7;
-        
-        for (auto &[eff, spd] : engineers) {
-            
-            speedSum += spd;
-            minHeap.push(spd);
-            
+        int MOD = 1e9 + 7;
+
+        for (auto engineer : engineers) {
+
+            // Add current engineer's speed
+            speedSum += engineer.second;
+            minHeap.push(engineer.second);
+
+            // If team size exceeds k, remove the smallest speed
             if (minHeap.size() > k) {
                 speedSum -= minHeap.top();
                 minHeap.pop();
             }
-            
-            ans = max(ans, speedSum * eff);
+
+            // Calculate performance
+            ans = max(ans, speedSum * engineer.first);
         }
-        
+
         return ans % MOD;
     }
 };
