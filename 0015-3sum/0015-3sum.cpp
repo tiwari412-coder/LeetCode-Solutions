593e7ce61728490f0ae16a1758a300764d4c18d5
tiwari412-coder <bcstiwari412@gmail.com>
2026-07-26 23:43:48 +0530

    Time: 47 ms (64.46%), Space: 29 MB (73.63%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
 
         vector<vector<int>> ans;
 
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size()-2;i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
             int k = nums.size()-1;
