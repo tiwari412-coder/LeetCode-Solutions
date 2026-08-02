76c429864ae0aadc3362765fe27e583e62798dbf
tiwari412-coder <bcstiwari412@gmail.com>
2026-08-03 00:35:22 +0530

    Time: 60 ms (7.95%), Space: 52.4 MB (5.39%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        vector<pair<int ,int>> ans;
+
+        unordered_map<int ,int>mp;
+        for(int x : nums){
+            mp[x]++;
+        }
+
+        for(int i=0 ; i<nums.size(); i++){
+            if(nums[i] % 2== 0){
+                ans.push_back({nums[i] , mp[nums[i]]});
+         }
+        }
+
+        sort(ans.begin() , ans.end());
+
+        int maxi = INT_MIN;
+        for(auto it : ans){
+            maxi = max(maxi , it.second);   // frequency store kiya
+        }
+
+        int value = -1;
+        for(auto p : ans){
+            if(maxi == p.second){
+                value = p.first;
+                break;
+            }
+        }
+        return value;
+    }
+};
