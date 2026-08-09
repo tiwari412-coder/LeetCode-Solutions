8b1b3edb7923a1fa155195415512e0f9acb20737
tiwari412-coder <bcstiwari412@gmail.com>
2026-08-10 01:58:20 +0530

    Time: 44 ms (24.78%), Space: 32 MB (32.54%) - LeetHub

---
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        unordered_map<char ,int>mp;
+        for(char ch : s){
+            mp[ch]++;
+        }
+        for(char ch : t){
+            mp[ch]--;
+        }
+
+        int total = 0;
+        for(auto it : mp){
+            if(it.second > 0 || it.second < 0){
+                total += abs(it.second);
+            }
+        }
+        return total;
+    }
+};
