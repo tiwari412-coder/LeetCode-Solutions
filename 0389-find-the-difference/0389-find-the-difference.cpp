06061eb080222a9ef85403bdcfbd17ee054c5556
Abhishek Tiwari <bcstiwari412@gmail.com>
2026-08-31 20:24:22 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.25%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for(char ch : s) ans ^= ch;
+
+        for(char ch : t) ans ^= ch;
+
+        return ans;
+    }
+};
