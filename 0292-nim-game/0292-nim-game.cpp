0797309a8dac305ef9c61c2e6f2648dde00bd3b8
Abhishek Tiwari <bcstiwari412@gmail.com>
2026-09-01 19:44:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.04%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n == 1 || n == 2 || n == 3) return true;
+        else if(n % 4 == 0) return false;
+        return true;
+     }
+};
