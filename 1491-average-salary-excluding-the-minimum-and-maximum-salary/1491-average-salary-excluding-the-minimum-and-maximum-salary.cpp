9351579151ea0b7a8dff1ca83b9e797fa9aa16a1
Abhishek Kumar Tiwari <bcstiwari412@gmail.com>
2026-09-15 19:22:28 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.61%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin() , salary.end());
+        double sum = 0;
+        int n = salary.size() - 2;
+
+        for(int i=1; i<salary.size()-1; i++){
+            sum += salary[i];
+        }
+
+        double avg = sum / n;
+
+        return avg;
+    }
+};
