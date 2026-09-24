4ef9e5c4bbb1d2eca27a17af3f4579128920ad78
Abhishek Kumar Tiwari <bcstiwari412@gmail.com>
2026-09-24 19:09:48 +0530

    Time: 488 ms (55.65%), Space: 244 MB (23.53%) - LeetHub

---
+++ 3551-minimum-swaps-to-sort-by-digit-sum/3551-minimum-swaps-to-sort-by-digit-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+     vector<pair<int,int>>ans;
+     vector<int>arr;
+
+     for(int i=0; i<nums.size(); i++){
+            int sum = 0;
+            int temp = nums[i];
+            while(temp > 0){
+                int rem = temp % 10;
+                sum += rem;
+                temp /= 10;
+            }
+            ans.push_back({nums[i] , sum});
+            arr.push_back(nums[i]);
+        }
+     
+
+     sort(ans.begin() , ans.end() ,[](const pair<int,int>& a , const pair<int,int>& b){
+        if(a.second == b.second) return a.first < b.first;
+        return a.second < b.second;
+     });
+
+    unordered_map<int,int>mp;
+    for(int i=0; i<nums.size(); i++) mp[nums[i]] = i;
+
+    int swaps = 0;
+
+    for(int i=0; i<nums.size(); i++){
+        if(ans[i].first == nums[i]) continue;
+
+        // find the position of ans array element where it is shifted and swap with the currect nums array value
+
+        int j = mp[ans[i].first];
+        swap(nums[i] , nums[j]);
+        swaps++;
+
+        // also update the position the map (which will store the index in correct order) that will represent that this index value of the arrays aana chahiye aur vahi swaps ho jayega
+
+        mp[nums[j]] = j;
+        mp[nums[i]] = i;
+    }
+
+    return swaps;
+    }
+};
